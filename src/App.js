5dbefa07b1a02ca7ebd58b2e25914a011c793427
Akunbekovv <akunbekovmaksatt@gmail.com>
2023-03-21 16:58:39 +0600

    done

---
+++ src/App.js
@@ -0,0 +1,26 @@
+import './App.css';
+import {BrowserRouter, Routes, Route} from "react-router-dom";
+import Transition from "./components/transitionPage/Transition";
+import First from "./pages/firstPage/First";
+import Second from "./pages/secondPage/Second";
+import Third from "./pages/thirdPage/Third";
+import Fourth from "./pages/fourthPage/Fourth";
+import Fifth from "./pages/fifthPage/Fifth";
+import {useEffect} from "react";
+import {useState} from "react";
+function App() {
+  return (
+    <BrowserRouter>
+      <Transition/>
+     <Routes>
+       <Route index element={<First/>}/>
+         <Route path="/second" element={<Second/>}/>
+         <Route path="/third" element={<Third/>}/>
+         <Route path="/fourth" element={<Fourth/>}/>
+         <Route path="fifth" element={<Fifth/>}/>
+     </Routes>
+    </BrowserRouter>
+  );
+}
+
+export default App;
